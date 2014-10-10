03e1b60cdd5cfab693fed16ab8bd783e3164fe62
mikemillspdx <mikemillspdx@gmail.com>
2014-10-09 20:10:19 -0700

    makingitlegtpart1

---
+++ server.js
@@ -0,0 +1,8 @@
+var restify = require('restify');
+var siteLoader = require('./scripts/server/lib/controllers/siteLoader.js');
+
+var server = restify.createServer();
+var port = process.env.PORT || 1337;
+server.get('/.*', siteLoader.loadsite);
+
+server.listen(port, function() { console.log('%s listening at %s', server.name, server.url); });
